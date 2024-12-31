af2a3ed56f1b665a5e996c3326fe8facc66463f1
tremar thomas <81628964+saltsphinx@users.noreply.github.com>
2024-12-30 21:29:14 -0600

    Add await to bcrypt compare function

--- config/passport.js
+++ config/passport.js
@@ -18,7 +18,7 @@ passport.use(
         return done(null, false);
       }
 
-      const match = bcrypt.compare(password, user.password);
+      const match = await bcrypt.compare(password, user.password);
       if (!match) {
         return done(null, false);
       }
